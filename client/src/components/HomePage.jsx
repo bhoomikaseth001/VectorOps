ed4b9beebd4238923fa4dd53188420483fc48c2a
Bhoomika Seth <bhoomikaseth999@gmail.com>
2026-07-12 11:13:30 +0530

    client side added Sun, Jul 12, 2026 11:13:29 AM

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * Basic Home Page Component
+ * Renders a clean, blank page with the text "Home Page" centered on the screen.
+ */
+export default function HomePage() {
+    return (
+        <div>
+            Home Page
+        </div>
+    );
+}
